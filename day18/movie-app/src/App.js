e044b97c22c24e9314fe148a42f5cef90120745a
RamR3R <sendtoramsundar@gmail.com>
2025-06-17 10:46:05 +0530

    Forms and List rendering

--- day18/movie-app/src/App.js
+++ day18/movie-app/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import AppHeader from './components/AppHeader';
 import CricketScore from './components/CricketScore';
+import MovieSearch from './components/MovieSearch';
 
 function App() {
   return (
     <div>
-      <CricketScore target={200} totalOvers={10}  />
+      {/* <CricketScore target={200} totalOvers={10}  /> */}
+      <MovieSearch />
       <br></br>
     </div>
   );
---
+++ day19/todo-app/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './Components/List';
+import Form from './Components/Form'; 
+import { useState } from 'react';
+
+function App() {
+
+  const [players , setPlayers] = useState([]);
+
+  // setPlayers([
+  //           {name : "Jaiswal", age :22}, 
+  //           {name : "Virat Kohli" , age : 38},
+  //           {name :  "Shubman Gill" , age : 25} , 
+  //           {name : "MSD" , age : 43}
+  //         ])
+  
+  const handleChange = ( newPlayer )=>{
+    
+    const newPlayersList = [...players];
+    newPlayersList.push(newPlayer);
+    setPlayers(newPlayersList);
+    console.log(newPlayersList);
+  }
+
+  return (
+    <div>
+      <List list={players} />
+      <Form handleChange={handleChange}  />
+    </div>
+  );
+}
+
+export default App;
