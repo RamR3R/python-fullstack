23196a91db0490cde5c08f22cf0d8aa0bb3bc089
RamR3R <sendtoramsundar@gmail.com>
2025-07-02 10:09:53 +0530

    stack imple with list

---
+++ day32/stack.py
@@ -0,0 +1,16 @@
+#stack DS
+
+stack = []
+
+print(stack.append(5)) #push function
+print(stack)
+stack.append(6)
+print(stack)
+stack.append(7)
+print(stack)
+print(stack[-1])
+print(stack)
+print(stack.pop()) #pop function
+print(stack)
+print(stack[-1]) # peak function
+print(stack)
