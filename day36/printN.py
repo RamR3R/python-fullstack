ca8698ac4d5f0788066d4564d70d46e2e8fd633a
RamR3R <sendtoramsundar@gmail.com>
2025-07-07 13:07:31 +0530

    print n from n to 1

--- day36/printN.py
+++ day36/printN.py
@@ -1,10 +1,10 @@
 def printer(n):
     #Base Condition
     if n == 0: return
-    #operation
-    print(n)
     # recursion call
     printer(n-1)
+    #operation
+    print(n)
 
 #main code 
 printer(5)
