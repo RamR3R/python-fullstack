efc85f893d30a8ca4e1d26a1f1119a5315472d8e
RamR3R <sendtoramsundar@gmail.com>
2025-07-10 15:18:59 +0530

    adj matrix representation is added

--- day39/graph.py
+++ day39/graph.py
@@ -16,3 +16,19 @@ for i in edges:
     adj[v].append(u)
 
 print("\n",adj)
+n = 4
+edges = [(1,2,2) , (2,3,3) , (3,4,4)]
+
+adjMatrix = [ [0] * (n+1) for i in range(n+1)]
+
+for i in edges:
+    u , v , w = i
+    adjMatrix[u][v] = w
+    #undirected graph
+    # adjMatrix[v][u] = w
+
+for row in adjMatrix:
+    print(row)
+
+
+edges = [(1,2,2) , (2,3,1) , (4,3,1) , (1,4,1)]
